4b43bd96fc58386ec15399d7cbbf640975d5ec18
AliReza Beigy <alireza.beigy.rb@gmail.com>
2024-05-03 20:34:09 +0330

    fix: mode exported enum

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export {default} from './GlobalDateTimePicker';
 export {CalendarType} from './Providers/DateTimePickerProvider';
-export {PickerMode} from './Providers/DateTimePickerProvider';
+export {DataTimePickerMode} from './Providers/DateTimePickerProvider';
 export {default as DateTimePickerThemes} from './Utilities/Themes';
 export {weekDaysJalali, yearMonthsJalali} from './Utilities/Contants';
 export {default as DateTimePickerTranslations} from './Utilities/Translations';
